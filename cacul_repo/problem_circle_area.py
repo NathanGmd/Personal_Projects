9a16ddd69a8c2bb7b2f49d5a883ec32e4db065b3
nathan germond <germond.nathan@gmail.com>
2024-07-07 11:37:28 +0200

    maj

---
+++ cacul_repo/problem_circle_area.py
@@ -0,0 +1,15 @@
+import math
+
+# def find_y():
+#     y_length = 0.1
+#     while math.sqrt( ( 4 ** 2 ) - y_length ** 2 ) != math.sqrt( ( 2 ** 2 ) - y_length ** 2 ):
+#         y_length += 0.01
+#     y = y_length
+#     return y
+
+# print ("y : ", find_y())
+
+
+result = math.sqrt( ( 4 ** 2 ) - 0.1 ** 2 )
+
+print (result)
